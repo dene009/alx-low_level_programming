3a81ddd27b75b50fa8634920dc0c4b8169f2f79f
dene009 <esidene009@gmail.com>
2022-09-08 18:48:02 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
  * main-Entry point 
  *
- * Author: Esidene
+ * Author:Esidene
  *
- * Return: Always 0 (success)
+ * Return:Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
