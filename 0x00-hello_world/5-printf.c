06cf4ac771e9ce2417525ac5ed42d4f34522d015
dene009 <esidene009@gmail.com>
2022-09-08 17:32:48 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,10 @@
  * but the outcome is a piece of art,
  *
  * Return: Always 0 if exited properly, non-zero otherwise
+ *
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
