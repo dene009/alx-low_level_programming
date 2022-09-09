5dda5a8e37e9625723ddacbd0d7e9ca406e965db
dene009 <esidene009@gmail.com>
2022-09-08 18:43:41 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main-Write a C program that prints exactly
+ *
+ * author:Esidene
+ *
+ * Return:Always 1 (success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+
+	Return (1);
+}
