fbeb0ef7fe702dcc7e73f3712d90a4d01e2a752f
dene009 <esidene009@gmail.com>
2022-09-11 13:26:02 -0700

    Task 8. UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
 #include <unistd.h>
-#include <stdio.h>
- 
+
 /**
- * main-Write a C program that prints exactly
+ * main-print the string in the put function
  *
- * author:Esidene
- *
- * Return:Always 1 (success)
+ * Return: 0
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
-
-	Return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
