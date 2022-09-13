aca0aa82fa6ff253b6b06b0b5d0e022ea6cc2bd2
dene009 <esidene009@gmail.com>
2022-09-13 06:56:26 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  prints alx, followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar('\n');
+
+	return (0);
+}
