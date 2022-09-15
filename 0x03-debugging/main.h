9aa2433158fc87fd49d762488edb530638030c70
dene009 <esidene009@gmail.com>
2022-09-15 06:24:56 -0700

    0. Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+int largest_nagative(int, int, int);
+void print_remaining_days(int, int, int);
+in covert_day(int, int);
+
+#endif /* MAIN_H */
