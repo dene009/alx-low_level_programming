43603b178dbfbcc0795fb473886fc08a8cb1f2ae
dene009 <esidene009@gmail.com>
2022-09-19 03:21:30 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
- * @s: input string.
- * Return: no return.
+ * _puts - prints a string to stdout
+ * @str: pointer parameter
  */
-void print_rev(char *s)
+void _puts(char *str)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
+	while (str[i] != '\0')
 	{
-		if (s[count] == '\0')
-			break;
-		count++;
+		_putchar(str[i]);
+
+		i++;
 	}
 
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
 	_putchar('\n');
 }
