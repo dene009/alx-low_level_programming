b8bf6b09bb4f91c0dcc326822e86ed9b6bc29391
dene009 <esidene009@gmail.com>
2022-10-01 00:10:12 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,11 +14,13 @@ int main(int argc, char *argv[])
 	int position, total, change, aux;
 
 	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+
 	position = total = change = aux = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
 
@@ -27,6 +29,7 @@ int main(int argc, char *argv[])
 	if (total <= 0)
 	{
 		printf("0\n");
+
 		return (0);
 	}
 
