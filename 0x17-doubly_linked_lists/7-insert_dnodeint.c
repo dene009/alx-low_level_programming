2f7fb5400a11e67a9fa85d6c91be2d96c1ee0a5b
dene009 <esidene009@gmail.com>
2022-12-14 21:46:26 -0800

    all task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
+
 	dlistint_t *head;
 
 	unsigned int i;
@@ -19,13 +20,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = NULL;
 
 	if (idx == 0)
+
 		new = add_dnodeint(h, n);
+
 	else
+
 	{
+
 		head = *h;
 		i = 1;
 
 		if (head != NULL)
+
 			while (head->prev != NULL)
 				head = head->prev;
 
@@ -35,8 +41,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			{
 				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
+
 				else
+
 				{
+
 					new = malloc(sizeof(dlistint_t));
 
 					if (new != NULL)
@@ -48,9 +57,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 						head->next = new;
 					}
 				}
+
 				break;
+
 			}
+
 			head = head->next;
+
 			i++;
 		}
 	}
