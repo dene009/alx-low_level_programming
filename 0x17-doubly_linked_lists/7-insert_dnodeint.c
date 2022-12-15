6fb602e22cc1849722c1cbc28e7ae656327f3f50
dene009 <esidene009@gmail.com>
2022-12-14 22:02:06 -0800

    completed Task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,13 +12,11 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
-
 	dlistint_t *head;
 
 	unsigned int i;
 
 	new = NULL;
-
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
